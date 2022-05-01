cc296a16bc4c2f6081c2258301548bc7dce757dd
MollyXuemn <Molly504954366@gmail.com>
2022-05-01 14:32:48 +0200

    all

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,7 +9,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "/"; /* change */
+  "https://github.com/MollyXuemn/mollyReactfFile/blob/master/src/Assets/XueMeini_CV.pdf"; /* change */
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
